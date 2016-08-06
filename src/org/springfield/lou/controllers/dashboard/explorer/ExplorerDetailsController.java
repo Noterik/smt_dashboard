be5b1742ac3691381bd97e22b971fe864500c885
Daniel Ockeloen <daniel@noterik.nl>
2016-08-06 12:21:21 +0200

    added delete and save buttons for properties

--- src/org/springfield/lou/controllers/dashboard/explorer/ExplorerDetailsController.java
+++ src/org/springfield/lou/controllers/dashboard/explorer/ExplorerDetailsController.java
@@ -27,6 +27,7 @@ public class ExplorerDetailsController extends Html5Controller {
 		selector = s;
 		screen.loadStyleSheet("dashboard/explorer/explorerdetails/explorerdetails.css");
 		fillPage();
+ 		screen.get(".explorerdetailssubmit").on("keypress","onKey", this);
   	}
 	
     public void onKey(Screen s,JSONObject data) {
@@ -38,6 +39,7 @@ public class ExplorerDetailsController extends Html5Controller {
 	
     public void onSave(Screen s,JSONObject data) {
     	String fieldname = (String)data.get("id");
+    	System.out.println("FIELDNAME="+fieldname);
     	String value = (String)data.get("value");
    		String path = model.getProperty("/screen/explorerpath");
     	model.setProperty(path+"/"+fieldname, value);
@@ -65,16 +67,33 @@ public class ExplorerDetailsController extends Html5Controller {
 			// do we want to delete the node ?
 			if (model.getProperty("/screen/deleterequest")!=null && model.getProperty("/screen/deleterequest").equals("true")) {
 				data.put("deleterequest","true");
+				String dp = model.getProperty("/screen/deleteproperty");
+				if (dp!=null && !dp.equals("")) data.put("deleteproperty",dp);
 		    	model.setProperty("/screen/deleterequest","false"); // reset the flag
 			}
    			screen.get(selector).parsehtml(data);       		
     		
      		screen.get(".explorerdetailssubmit").on("change","onSave", this);
+       		screen.get(".explorerpropertydelete").on("mouseup","onDeletePropertyRequest", this);
+       		screen.get(".explorerpropertydeleteno").on("mouseup","onDeletePropertyRequestNo", this);
+       		screen.get(".explorerpropertydeleteyes").on("mouseup","onDeletePropertyRequestYes", this);
+	 		screen.get("#explorerdetailsdelete").on("mouseup","onDeleteRequest", this);
+	 		screen.get("#explorerdetailsdeleteyes").on("mouseup","onDeleteRequestYes", this);
+	 		screen.get("#explorerdetailsdeleteno").on("mouseup","onDeleteRequestNo", this);
 	 		screen.get("#explorerdetailsdelete").on("mouseup","onDeleteRequest", this);
 	 		screen.get("#explorerdetailsdeleteyes").on("mouseup","onDeleteRequestYes", this);
 	 		screen.get("#explorerdetailsdeleteno").on("mouseup","onDeleteRequestNo", this);
      		screen.get("#newsave").on("mouseup","newname,newvalue","onNewSave", this);
 	}
+	
+    public void onDeletePropertyRequest(Screen s,JSONObject data) {
+    	String fieldname = (String)data.get("id");
+    	fieldname = fieldname.substring(0,fieldname.length()-6);
+    	System.out.println("FIELDNAME="+fieldname);
+    	model.setProperty("/screen/deleterequest","true");
+    	model.setProperty("/screen/deleteproperty",fieldname);
+    	fillPage();
+    }
 	
     public void onDeleteRequest(Screen s,JSONObject data) {
     	model.setProperty("/screen/deleterequest","true");
@@ -84,6 +103,18 @@ public class ExplorerDetailsController extends Html5Controller {
     public void onDeleteRequestNo(Screen s,JSONObject data) {
     	fillPage();
     }
+    
+    public void onDeletePropertyRequestNo(Screen s,JSONObject data) {
+    	fillPage();
+    }
+    
+    public void onDeletePropertyRequestYes(Screen s,JSONObject data) {
+    	String path = model.getProperty("/screen/explorerpath")+"/properties/"+model.getProperty("/screen/deleteproperty");
+    	Fs.deleteNode(path); // we use delete node since properties are really nodes, not sure if we should build this in model
+    	fillPage();
+    }
+    
+    
     
     public void onDeleteRequestYes(Screen s,JSONObject data) {
 		String path = model.getProperty("/screen/explorerpath");
